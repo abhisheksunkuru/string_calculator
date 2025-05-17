f618f958118119eb9ddb1cc6cd37817ba76ae21a
Abhishek <abhisheksunkuru@gmail.com>
2025-05-17 13:34:44 +0530

    Add unit tests forscenarios

---
+++ spec/calculator_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../calculator'
+RSpec.describe StringCalculator do
+  describe "#add" do
+    it "should return 0 for empty string" do
+    end
+    it "should return same number for single number"  do
+    end
+    it "should return sum for comma separated numbers" do
+    end  
+    it "should return sum for  multiline numbers separated by comma" do
+    end
+    it "should support custom delimiters" do
+    end  
+    it "should throw error for negative number" do
+    end  
+  end
+end  
